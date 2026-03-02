e6e77444cdfd0fc96f204e66fa8da75a346d5f08
BT <83559291+takebri@users.noreply.github.com>
2026-03-02 09:57:23 -0800

    Removed aggregation

--- sql_queries/02_query_top_conditions.sql
+++ sql_queries/02_query_top_conditions.sql
@@ -6,10 +6,10 @@
 
 SELECT 
     chronic_condition,
-    SUM(member_count) as total_members,
-    AVG(med_oop_member) as avg_median_oop_cost,
-    AVG(med_claim_ct) as avg_median_claims,
-    AVG(med_oop_member) / NULLIF(AVG(med_claim_ct), 0) as oop_cost_per_claim
+    member_count as total_members,
+    med_oop_member as median_oop_cost,
+    med_claim_ct as median_claims,
+    med_oop_member / NULLIF(med_claim_ct, 0) as oop_cost_per_claim
 FROM CA_HEALTH.PUBLIC.HPD_OOP_CHRONIC
 WHERE 
     county = 'All'
@@ -17,6 +17,5 @@ WHERE
     AND chronic_condition IS NOT NULL
     AND chronic_number_v2 = 'All'  -- CHANGED: was IS NULL, now = 'All'
     AND member_count > 0
-GROUP BY chronic_condition
-ORDER BY avg_median_oop_cost DESC;
+ORDER BY median_oop_cost DESC;
 
